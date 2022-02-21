2bc2a65ca2ac0885d3458b116eb533b99223ffa8
CalebEskew <99913483+CalebEskew@users.noreply.github.com>
2022-02-21 12:05:21 -0500

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Bank
+{
+
+   
+
+
+    public Account[] accountsArray = new Account[10];
+
+   
+        //add account method
+   
+    public bool AddAccount(Account newAcc)
+    {
+        bool accAdded = false;
+
+        for (int i = 0; i < accountsArray.Length; i++)
+        {
+            if (accountsArray[i] == null)
+            {
+               
+                accountsArray[i] = newAcc;
+                accAdded = true;
+                break;
+            }
+
+
+        }
+
+        return accAdded;
+
+
+    }
+
+    public void ApplyDeposits()
+    {
+        for (int i = 0; i < accountsArray.Length; i++)
+        {
+            if (accountsArray[i] != null)
+            {
+                accountsArray[i].UpdateAcctBalance();
+            }
+        }
+
+
+    }
+
+    public override string ToString()
+    {
+        string str = "";
+        for (int i = 0; i < accountsArray.Length; i++)
+        {
+            if (accountsArray[i] != null)
+            {
+                str += accountsArray[i] + "\n\n";
+            }
+            else
+            {
+                str += "No Account found \n\n";
+            }
+        }
+
+        return str;
+    }
+
+}
